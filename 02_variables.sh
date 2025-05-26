b52a0a63fc66e5ce4cfba8d9df1f252e307f6844
Sharath3116 <sharath3192@gmail.com>
2025-05-26 14:00:25 +0530

    no central repo

---
+++ 02_variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+Name1=FUKC
+Name2=sUCK
+
+echo "tell me about your self $Name1; then whats the matter"
+echo "i'm devosp lead in $Name2 softwares" $Name1
+echo "hakjdhkqjwdhq $Name1"
+
+
+DATE=$(date)
+
+echo "script staterd $DATE"
--- scripts.sh
+++ scripts.sh
@@ -1,4 +1,6 @@
-#!bin/bash
+#!/bin/bash
 
-print "hello world"
+echo help me
+
+echo "hello fucking guys"
 
